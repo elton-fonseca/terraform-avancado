32a378b3459e431cde5ff0385461582ddc981e5d
Elton Fonseca <elton869@gmail.com>
2022-04-02 10:47:21 -0300

    00 - Estrutura inicial do projeto

---
+++ main.tf
@@ -0,0 +1,24 @@
+output "ola_mundo" {
+  value = "Olá Treinaweb!"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+module "webservers" {
+  source = "./webservers"
+}
+
+module "apiservers" {
+  source = "./apiservers"
+}
