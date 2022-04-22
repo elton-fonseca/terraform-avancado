9002786becc07f863a73f920f0463a826daa1718
Elton Fonseca <elton869@gmail.com>
2022-04-22 16:34:24 -0300

    12 - Conhecendo o bloco Locals

--- apiservers/main.tf
+++ apiservers/main.tf
@@ -1,6 +1,20 @@
-resource "aws_instance" "api_server" {
+locals {
+  tipo_instancia = "t2.${var.tipo_instancia}"
+}
+
+resource "aws_instance" "api_server1" {
   ami           = data.aws_ami.windows.id
-  instance_type = "t2.${var.tipo_instancia}"
+  instance_type = local.tipo_instancia
+  cpu_core_count = var.quantidade_cpus
+
+  tags = {
+    Name = var.etiquetas["Name"]
+  }
+}
+
+resource "aws_instance" "api_server2" {
+  ami           = data.aws_ami.windows.id
+  instance_type = local.tipo_instancia
   cpu_core_count = var.quantidade_cpus
 
   tags = {
