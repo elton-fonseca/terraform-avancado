f5ad1e8daa96623e4ca8e52ecaebc971090c130a
Elton Fonseca <elton869@gmail.com>
2022-04-22 15:41:29 -0300

    8 - Conhecendo os tipos de dados

--- apiservers/variaveis.tf
+++ apiservers/variaveis.tf
@@ -3,3 +3,23 @@ variable "tipo_instancia" {
   type = string
   default = "t2.micro"
 }
+
+variable "quantidade_cpus" {
+  description = "Define a quantidade CPUs de uma maquina"
+  type = number
+  default = 1
+}
+
+variable "plataformas" {
+  description = "Define a plataforma do sistema operacional"
+  type = list
+  default = [ "windows" ]
+}
+
+variable "etiquetas" {
+  description = "Define as tags de uma maquina virtual"
+  type = map
+  default = {
+    Name = "ServidorSistemaAPI"
+  }
+}
